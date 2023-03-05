fed7700ca0cb30d5c7e0f69c060cf905bb74ad25
Ynverxe <nahubar65@gmail.com>
2023-03-05 18:13:03 -0300

    fix: adapt string data parsing to the ModelDataValue parameter

--- open-nexus-api/src/main/java/com/github/ynverxe/translation/resource/mapping/StringInterpreter.java
+++ open-nexus-api/src/main/java/com/github/ynverxe/translation/resource/mapping/StringInterpreter.java
@@ -1,15 +1,31 @@
 package com.github.ynverxe.translation.resource.mapping;
 
+import com.github.ynverxe.structured.data.ModelDataList;
+import com.github.ynverxe.structured.data.ModelDataValue;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collections;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StringInterpreter implements ResourceInterpreter {
     @Override
-    public @Nullable Object buildResource(@NotNull Map<String, Object> data, @NotNull FormattingScheme formattingScheme, @NotNull FormattingContext formattingContext, @NotNull ResourceMapper resourceMapper) {
-        String str = String.valueOf(data.values().toArray(new Object[] {})[0]);
-        return resourceMapper.formatStrings(Collections.singletonList(str), formattingScheme, formattingContext);
+    public @Nullable Object buildResource(
+            @NotNull ModelDataValue value,
+            @NotNull FormattingScheme formattingScheme,
+            @NotNull FormattingContext formattingContext,
+            @NotNull ResourceMapper resourceMapper
+    ) {
+        List<String> values = new ArrayList<>();
+
+        if (value.isList()) {
+            ModelDataList list = value.asList();
+
+            list.forEach(dataValue -> values.add(dataValue.toString()));
+        } else {
+            values.add(value.toString());
+        }
+
+        return resourceMapper.formatStrings(values, formattingScheme, formattingContext);
     }
 }
